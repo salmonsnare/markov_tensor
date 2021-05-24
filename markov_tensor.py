a19d5243d5763897e4b66875b34b42858a02fc11
Tadachika Oki <sakesnare@gmail.com>
2021-05-24 20:01:37 +0900

    恒等射を実相

--- markov_tensor.py
+++ markov_tensor.py
@@ -5,10 +5,12 @@ FXTens の Python による表現
 ストランドの重みを辞書として表現。
   tensor_x = {
     "profile": [[2], [2]],  
-    "[[1], [1]]": 0.3, 
-    "[[1], [2]]": 0.7, 
-    "[[2], [1]]": 0.5, 
-    "[[2], [2]]": 0.5, 
+    "strands" {
+        "[[1], [1]]": 0.3, 
+        "[[1], [2]]": 0.7, 
+        "[[2], [1]]": 0.5, 
+        "[[2], [2]]": 0.5
+    }
   }
 
 結合演算
@@ -18,8 +20,13 @@ FXTens の Python による表現
 
 import numpy as np
 import pandas as pd
+from fractions import Fraction
+import itertools
 
 DEBUG = True
+
+DOMAIN_PROFILE = 0
+CODOMAIN_PROFILE = 1
 
 def get_lattice_points(strand):
     """
@@ -39,7 +46,7 @@ def check_composable(tensor_x, tensor_y):
     @param tensor_x テンソル
     @param tensor_y テンソル
     """
-    return True if tensor_x["profile"][1] == tensor_y["profile"][0] else False
+    return True if tensor_x["profile"][CODOMAIN_PROFILE] == tensor_y["profile"][DOMAIN_PROFILE] else False
 
 
 def is_markov(tensor):
@@ -48,42 +55,70 @@ def is_markov(tensor):
     @param tensor
     """
     total = {}
-    for strand in [item for item in list(tensor.keys()) if item != "profile"]:
+    total_strands = {}
+    total["strands"] = {}
+    for strand in list(tensor["strands"].keys()):
         strand_from, _ = get_lattice_points(strand)
         strand_from_str = str(strand_from)
-        if strand_from_str in total:
-            total[strand_from_str] += tensor[strand]
+        if strand_from_str in total_strands.keys():
+            total_strands[strand_from_str] += tensor["strands"][strand]
         else:
-            total[strand_from_str] = tensor[strand]
+            total_strands[strand_from_str] = tensor["strands"][strand]
+    total["strands"] = total_strands
     ret = True
-    for strand_from_str in total.keys():
-        ret = ret and (total[strand_from_str] == 1.0)
+    for strand_from_str in total["strands"].keys():
+        ret = ret and (total["strands"][strand_from_str] == 1.0)
     if ret:
         print("this tensor is Markov")
 
     return ret
 
 
-def composition_process(tensor_x, tensor_y, strand_x, strand_y, tensor_result):
+def create_n_bar(n):
+    return [index + 1 for index in range(n)]
+
+
+def create_indexies(base_list):
+    return [list(item) for item in list(itertools.product(*base_list))] # *base_list で引数としてリストを unpack して渡す。
+
+
+def identity(tensor):
+    tensor_result = {}
+    profile = tensor["profile"]
+    tensor_result["profile"] = profile
+    tensor_result["strands"] = {}
+    domain = profile[DOMAIN_PROFILE]
+    codomain = profile[CODOMAIN_PROFILE]
+
+    domain_base_list = [create_n_bar(domain_item) for domain_item in domain]
+    codomain_base_list = [create_n_bar(codomain_item) for codomain_item in codomain]
+
+    for item in itertools.product(create_indexies(domain_base_list), create_indexies(codomain_base_list)):
+        tensor_result["strands"][str(list(item))] = 1
+
+    return tensor_result
+      
+
+def composition_process(tensor_x, tensor_y, strand_x, strand_y, strands_result, tensor_result):
     strand_from_x, strand_to_x = get_lattice_points(strand_x)
     strand_from_y, strand_to_y = get_lattice_points(strand_y)
     # 結合演算の結果のストランドの重みを算出
-    if strand_to_x == strand_from_y:  # tensor_x のあるストランドの終点と、tensor_y のあるストランドの始点が一致したばあい
+    if strand_to_x == strand_from_y:  # tensor_x のあるストランドの終点と、tensor_y のあるストランドの始点が一致した場合
         # 結合演算の結果のストランドの始点と終点の設定
         # キー strand_result はストランドの始点と終点を表す格子点を表す。
-        strand_result = str([strand_from_x, strand_to_y])
-        mult = round(tensor_x[strand_x] * tensor_y[strand_y], 5)
+        strand_lattice_points = str([strand_from_x, strand_to_y])
+        mult = round(tensor_x["strands"][strand_x] * tensor_y["strands"][strand_y], 5)
         if DEBUG:
             print("---")
-            print("  strand_from_x: {0}, strand_to_x: {1}, tensor_x[strand_x]: {2}".format(strand_from_x, strand_to_x, tensor_x[strand_x]))
-            print("  strand_from_y: {0}, strand_to_y: {1}, tensor_y[strand_y]: {2}".format(strand_from_y, strand_to_y, tensor_y[strand_y]))
-            print("tensor_x[strand_x] * tensor_y[strand_y]: {0}".format(mult))
-        if strand_result in tensor_result.keys(): # もし既にキー strand_result に値が設定されていれば加算
-            tensor_result[strand_result] += mult
+            print("  strand_from_x: {0}, strand_to_x: {1}, tensor_x[strand_x]: {2}".format(strand_from_x, strand_to_x, tensor_x["strands"][strand_x]))
+            print("  strand_from_y: {0}, strand_to_y: {1}, tensor_y[strand_y]: {2}".format(strand_from_y, strand_to_y, tensor_y["strands"][strand_y]))
+            print("tensor_x[strands][strand_x] * tensor_y[strands][strand_y]: {0}".format(mult))
+        if strand_lattice_points in strands_result.keys(): # もし既にキー strand_result に値が設定されていれば加算
+            strands_result[strand_lattice_points] += mult
         else: # もし既にキー strand_result に値が設定されていなければ設定
-            tensor_result[strand_result] = mult
+            strands_result[strand_lattice_points] = mult
 
-    return tensor_result
+    return tensor_result, strands_result
 
 
 def composition(tensor_x, tensor_y):
@@ -94,14 +129,18 @@ def composition(tensor_x, tensor_y):
     """
 
     tensor_result = {}
+    strands_result = {}
     if check_composable(tensor_x, tensor_y):
         tensor_result["profile"] = [
-            tensor_x["profile"][0], tensor_y["profile"][1]]
-        for strand_x in [item for item in list(tensor_x.keys()) if item != "profile"]:
-            for strand_y in [item for item in list(tensor_y.keys()) if item != "profile"]:
-                tensor_result = composition_process(tensor_x, tensor_y, strand_x, strand_y, tensor_result)
+            tensor_x["profile"][DOMAIN_PROFILE], 
+            tensor_y["profile"][CODOMAIN_PROFILE]
+        ]
+        for strand_x in [item for item in list(tensor_x["strands"].keys())]:
+            for strand_y in [item for item in list(tensor_y["strands"].keys())]:
+                tensor_result, strands_result = composition_process(tensor_x, tensor_y, strand_x, strand_y, strands_result, tensor_result)
     else:
         print("cannot compose")
+    tensor_result["strands"] = strands_result
 
     return tensor_result
 
@@ -124,12 +163,12 @@ def composition(tensor_x, tensor_y):
 def create_profile_tensor_product(tensor_x, tensor_y, tensor_result):
     # テンソル積のプロファイルを作成
     domain = []
-    domain.extend(tensor_x["profile"][0])
-    domain.extend(tensor_y["profile"][0])
+    domain.extend(tensor_x["profile"][DOMAIN_PROFILE])
+    domain.extend(tensor_y["profile"][DOMAIN_PROFILE])
 
     codomain = []
-    codomain.extend(tensor_x["profile"][1])
-    codomain.extend(tensor_y["profile"][1])
+    codomain.extend(tensor_x["profile"][CODOMAIN_PROFILE])
+    codomain.extend(tensor_y["profile"][CODOMAIN_PROFILE])
     tensor_result["profile"] = [
         domain,  # リストの連接によりプロファイルの域を作成 
         codomain # リストの連接によりプロファイルの余域を作成
@@ -137,7 +176,7 @@ def create_profile_tensor_product(tensor_x, tensor_y, tensor_result):
     return tensor_result
 
 
-def tensor_product_process(tensor_x, tensor_y, strand_x, strand_y, tensor_result):
+def tensor_product_process(tensor_x, tensor_y, strand_x, strand_y, strands_result, tensor_result):
     """
     テンソル積を算出するためのストランド間の計算
     """
@@ -152,17 +191,17 @@ def tensor_product_process(tensor_x, tensor_y, strand_x, strand_y, tensor_result
     strand_to.extend(strand_to_x)
     strand_to.extend(strand_to_y)
 
-    strand_result = str([strand_from, strand_to])
-    mult = round(tensor_x[strand_x] * tensor_y[strand_y], 5)
+    strand_lattice_points = str([strand_from, strand_to])
+    mult = round(tensor_x["strands"][strand_x] * tensor_y["strands"][strand_y], 5)
     if DEBUG:
         print("---")
-        print("  strand_from_x: {0}, strand_to_x: {1}, tensor_x[strand_x]: {2}".format(strand_from_x, strand_to_x, tensor_x[strand_x]))
-        print("  strand_from_y: {0}, strand_to_y: {1}, tensor_y[strand_y]: {2}".format(strand_from_y, strand_to_y, tensor_y[strand_y]))
+        print("  strand_from_x: {0}, strand_to_x: {1}, tensor_x[strands][strand_x]: {2}".format(strand_from_x, strand_to_x, tensor_x["strands"][strand_x]))
+        print("  strand_from_y: {0}, strand_to_y: {1}, tensor_y[strands][strand_y]: {2}".format(strand_from_y, strand_to_y, tensor_y["strands"][strand_y]))
         print("strand_from: {0}, strand_to: {1}".format(strand_from, strand_to))
-        print("tensor_x[strand_x] * tensor_y[strand_y]: {0}".format(mult))
-    tensor_result[strand_result] = mult
+        print("tensor_x[strands][strand_x] * tensor_y[stdands][strand_y]: {0}".format(mult))
+    strands_result[strand_lattice_points] = mult
 
-    return tensor_result
+    return tensor_result, strands_result
 
 
 def tensor_product(tensor_x, tensor_y):
@@ -179,11 +218,13 @@ def tensor_product(tensor_x, tensor_y):
         print_tensor(tensor_y)
 
     tensor_result = {}
+    strands_result = {}
     tensor_result = create_profile_tensor_product(tensor_x, tensor_y, tensor_result)
 
-    for strand_x in [item for item in list(tensor_x.keys()) if item != "profile"]:
-        for strand_y in [item for item in list(tensor_y.keys()) if item != "profile"]:
-            tensor_result = tensor_product_process(tensor_x, tensor_y, strand_x, strand_y, tensor_result)
+    for strand_x in [item for item in list(tensor_x["strands"].keys())]:
+        for strand_y in [item for item in list(tensor_y["strands"].keys())]:
+            tensor_result, strands_result = tensor_product_process(tensor_x, tensor_y, strand_x, strand_y, strands_result, tensor_result)
+    tensor_result["strands"] = strands_result
 
     return tensor_result
 
@@ -194,92 +235,108 @@ def print_tensor(tensor):
     @param tensor テンソル
     """
     print("---")
-    for key in tensor.keys():
-        print(key, tensor[key])
+    print("profile: ", tensor["profile"])
+    strands = tensor["strands"]
+    for key in strands.keys():
+        print(key, strands[key])
 
 
 def main():
     tensor_a = {
         "profile": [[2], [2, 2]],
-        "[[1], [1, 1]]": 0.25,
-        "[[1], [1, 2]]": 0.25,
-        "[[1], [2, 1]]": 0.25,
-        "[[1], [2, 2]]": 0.25,
-        "[[2], [1, 1]]": 0.15,
-        "[[2], [1, 2]]": 0.25,
-        "[[2], [2, 1]]": 0.35,
-        "[[2], [2, 2]]": 0.25
+        "strands": {
+            "[[1], [1, 1]]": Fraction(1, 4),
+            "[[1], [1, 2]]": Fraction(1, 4),
+            "[[1], [2, 1]]": Fraction(1, 4),
+            "[[1], [2, 2]]": Fraction(1, 4),
+            "[[2], [1, 1]]": Fraction(3, 20),
+            "[[2], [1, 2]]": Fraction(1, 4),
+            "[[2], [2, 1]]": Fraction(7, 20),
+            "[[2], [2, 2]]": Fraction(1, 4)
+        }
     }
 
     tensor_b = {
         "profile": [[2, 2], [2, 2]],
-        "[[1, 1], [1, 1]]": 0.25,
-        "[[1, 1], [1, 2]]": 0.15,
-        "[[1, 1], [2, 1]]": 0.35,
-        "[[1, 1], [2, 2]]": 0.25,
-        "[[1, 2], [1, 1]]": 0.25,
-        "[[1, 2], [1, 2]]": 0.25,
-        "[[1, 2], [2, 1]]": 0.25,
-        "[[1, 2], [2, 2]]": 0.25,
-        "[[2, 1], [1, 1]]": 0.25,
-        "[[2, 1], [1, 2]]": 0.25,
-        "[[2, 1], [2, 1]]": 0.25,
-        "[[2, 1], [2, 2]]": 0.25,
-        "[[2, 2], [1, 1]]": 0.25,
-        "[[2, 2], [1, 2]]": 0.25,
-        "[[2, 2], [2, 1]]": 0.25,
-        "[[2, 2], [2, 2]]": 0.25
+        "strands": {
+            "[[1, 1], [1, 1]]": Fraction(1, 4),
+            "[[1, 1], [1, 2]]": Fraction(3, 20),
+            "[[1, 1], [2, 1]]": Fraction(7, 20),
+            "[[1, 1], [2, 2]]": Fraction(1, 4),
+            "[[1, 2], [1, 1]]": Fraction(1, 4),
+            "[[1, 2], [1, 2]]": Fraction(1, 4),
+            "[[1, 2], [2, 1]]": Fraction(1, 4),
+            "[[1, 2], [2, 2]]": Fraction(1, 4),
+            "[[2, 1], [1, 1]]": Fraction(1, 4),
+            "[[2, 1], [1, 2]]": Fraction(1, 4),
+            "[[2, 1], [2, 1]]": Fraction(1, 4),
+            "[[2, 1], [2, 2]]": Fraction(1, 4),
+            "[[2, 2], [1, 1]]": Fraction(1, 4),
+            "[[2, 2], [1, 2]]": Fraction(1, 4),
+            "[[2, 2], [2, 1]]": Fraction(1, 4),
+            "[[2, 2], [2, 2]]": Fraction(1, 4)
+        }
     }
 
     tensor_domain_empty_list = {
         "profile": [[], [2]],
-        "[[], [1]]": 0.1,
-        "[[], [2]]": 0.9,
+        "strands": {
+            "[[], [1]]": Fraction(1, 10),
+            "[[], [2]]": Fraction(9, 10)
+        }
     }
 
     tensor_c = {
         "profile": [[2], [2]],
-        "[[1], [1]]": 0.3,
-        "[[1], [2]]": 0.7,
-        "[[2], [1]]": 0.5,
-        "[[2], [2]]": 0.5,
+        "strands": {
+        "[[1], [1]]": Fraction(3, 10),
+        "[[1], [2]]": Fraction(7, 10),
+        "[[2], [1]]": Fraction(1, 2),
+        "[[2], [2]]": Fraction(1, 2)
+        }
     }
 
     tensor_d = {
         "profile": [[2], [2]],
-        "[[1], [1]]": 0.4,
-        "[[1], [2]]": 0.6,
-        "[[2], [1]]": 0.1,
-        "[[2], [2]]": 0.9,
+        "strands": {
+            "[[1], [1]]": Fraction(2, 5),
+            "[[1], [2]]": Fraction(3, 5),
+            "[[2], [1]]": Fraction(1, 10),
+            "[[2], [2]]": Fraction(9, 10)
+        }
     }
 
     tensor_e = {
         "profile": [[1], [2, 2]], 
-        "[[1], [1, 1]]": 0.1, 
-        "[[1], [1, 2]]": 0.2, 
-        "[[1], [2, 1]]": 0.3, 
-        "[[1], [2, 2]]": 0.4
+        "strands": {
+            "[[1], [1, 1]]": Fraction(1, 10), 
+            "[[1], [1, 2]]": Fraction(2, 10), 
+            "[[1], [2, 1]]": Fraction(3, 10), 
+            "[[1], [2, 2]]": Fraction(4, 10)
+        }
     }
 
     tensor_f = {
         "profile": [[2], [2]], 
-        "[[1], [1]]": 0.5, 
-        "[[1], [2]]": 0.5, 
-        "[[2], [1]]": 0.5, 
-        "[[2], [2]]": 0.5
+        "strands": {
+            "[[1], [1]]": Fraction(1, 2), 
+            "[[1], [2]]": Fraction(1, 2), 
+            "[[2], [1]]": Fraction(1, 2), 
+            "[[2], [2]]": Fraction(1, 2)
+        }
     }
 
     # テンソル間の演算
     for tensor_result in [
-        composition(tensor_domain_empty_list, tensor_c), 
         composition(tensor_a, tensor_b), 
-        composition(composition(composition(tensor_c, tensor_d), tensor_d), tensor_d), 
-        tensor_product(tensor_c, tensor_d), 
-        tensor_product(tensor_domain_empty_list, tensor_d)
+        identity(tensor_a),  
+        # composition(tensor_domain_empty_list, tensor_c), 
+        # composition(composition(composition(tensor_c, tensor_d), tensor_d), tensor_d), 
+        # tensor_product(tensor_c, tensor_d), 
+        # tensor_product(tensor_domain_empty_list, tensor_d)
     ]:
-        is_markov(tensor_result)           # マルコフ性のチェック
+        is_markov(tensor_result)    # マルコフ性のチェック
         print_tensor(tensor_result) # テンソルを標準出力
-
 
 
 if __name__ == "__main__":
